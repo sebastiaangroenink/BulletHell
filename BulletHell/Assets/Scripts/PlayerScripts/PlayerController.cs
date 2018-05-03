d7bd62c8dd7bc45d49caa4a97594822d2da3e4a9
sebastiaangroeninkDeltion <sebastiaangroenink@gmail.com>
2018-05-03 21:05:49 +0200

    pushing new maps

---
+++ BulletHell/Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+	void Update () {
+
+
+        CheckMouse();
+	}
+
+    //Extra activation check to prevent errors
+    void CheckMouse()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            MovePlayer(true);
+        }
+    }
+    
+    //Moves player's character to user's mouse potion if Clicked.
+    void MovePlayer(bool moveMe)
+    {
+        if (moveMe)
+        {
+            transform.position = Input.mousePosition;
+        }
+    }
+}
