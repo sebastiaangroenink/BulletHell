5239e4b063db68d87ccb263c8800fa33dd93dd3d
GuyVanDerMeulenDeltion <guy_meulen@live.nl>
2018-05-06 04:35:51 +0200

    Finished Boss #5 Vulcano

---
+++ BulletHell/Assets/Scripts/Bosses/Projectiles/Bending.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bending : ProjectileTemplate {
+
+    [Header("Bending Settings:")]
+    public float bendingAngle = 10;
+    public float bendTime = 3;
+
+    #region Private Variables
+    private float timer;
+    #endregion
+
+    public override void Start() {
+        base.Start();
+        timer = bendTime;
+    }
+
+    public override void SetParameters () {
+        base.SetParameters();
+
+        bool canBend = CanBend();
+
+        bendTime -= Time.deltaTime;
+
+        if(canBend)
+        rotation.z += bendingAngle * Time.deltaTime;
+	}
+
+    private bool CanBend() {
+        if (bendTime <= 0)
+            return false;
+        else
+            return true;
+    }
+}
--- BulletHell/Assets/Scripts/Bosses/Scripts/Bosses/BossFour/Vulcano.cs
+++ BulletHell/Assets/Scripts/Bosses/Scripts/Bosses/BossFour/Vulcano.cs
@@ -6,19 +6,83 @@ public class Vulcano : BossTemplate {
 
     [Header("Vulcano Settings:")]
     public float rotationSpeed = 5;
-    public int bulletAmount = 3;
+    public int healthPhaseTwo = 1500;
 
+    [Header("Vulcano Projectile Settings:")]
+    public float bendForce = 40;
+    public float spacing = 5;
+    public float velocity = 10;
+    public float bendVelocity = 20;
+    public float bendProjTimerBase = 0.3f;
+
+
+    #region Private Variables
     private float angleOffset = 0;
+    private bool phaseTwo = false;
+    private float bendProjTimer;
+    #endregion
+
+    public override void Update() {
+        base.Update();
+
+        bendProjTimer -= Time.deltaTime;
+    }
+
+    public override void Start() {
+        base.Start();
+
+        bendProjTimer = bendProjTimerBase;
+    }
 
     public override void BaseAttack() {
         base.BaseAttack();
 
+        bool canFireBendingProjectiles = BendingProjectileCooldown();
+
+        if (health <= healthPhaseTwo)
+            phaseTwo = true;
+
         if (cooledDown) {
-            for (int i = 0; i < bulletAmount; i++) {
-                GameObject projectile = Instantiate(projectiles[0].gameObject, transform.position, Quaternion.identity);
-                projectile.GetComponent<ProjectileTemplate>().forwardAxis = angleOffset + rotationSpeed;
-                projectile.GetComponent<ProjectileTemplate>().forwardAxis = angleOffset + rotationSpeed;
+
+                    GameObject projectile = Instantiate(projectiles[0].gameObject, transform.position, Quaternion.identity);
+                    projectile.GetComponent<ProjectileTemplate>().movementSpeed = velocity;
+                    projectile.GetComponent<ProjectileTemplate>().forwardAxis = angleOffset + rotationSpeed;
+
+                    projectile = Instantiate(projectiles[0].gameObject, transform.position, Quaternion.identity);
+                    projectile.GetComponent<ProjectileTemplate>().movementSpeed = velocity;
+                    projectile.GetComponent<ProjectileTemplate>().forwardAxis = -angleOffset + rotationSpeed;
+
+                    projectile = Instantiate(projectiles[0].gameObject, transform.position, Quaternion.identity);
+                    projectile.GetComponent<ProjectileTemplate>().movementSpeed = velocity;
+                    projectile.GetComponent<ProjectileTemplate>().forwardAxis = -angleOffset + -rotationSpeed;
+
+                    projectile = Instantiate(projectiles[0].gameObject, transform.position, Quaternion.identity);
+                    projectile.GetComponent<ProjectileTemplate>().movementSpeed = velocity;
+                    projectile.GetComponent<ProjectileTemplate>().forwardAxis = +angleOffset + -rotationSpeed;
+
+
+                    angleOffset += spacing;
             }
+
+            if(phaseTwo && canFireBendingProjectiles) {
+            GameObject projectile = Instantiate(projectiles[1].gameObject, transform.position, Quaternion.identity);
+            projectile.GetComponent<ProjectileTemplate>().movementSpeed = bendVelocity;
+            projectile.GetComponent<ProjectileTemplate>().forwardAxis = rotationSpeed * Mathf.PI * angleOffset;
+            projectile.GetComponent<Bending>().bendingAngle = bendForce;
+
+            projectile = Instantiate(projectiles[1].gameObject, transform.position, Quaternion.identity);
+            projectile.GetComponent<ProjectileTemplate>().movementSpeed = bendVelocity;
+            projectile.GetComponent<ProjectileTemplate>().forwardAxis = -rotationSpeed * Mathf.PI * angleOffset;
+            projectile.GetComponent<Bending>().bendingAngle = -bendForce;
         }
+        }
+
+    public bool BendingProjectileCooldown() {
+        if (bendProjTimer <= 0) {
+            bendProjTimer = bendProjTimerBase;
+            return true;
+        }
+
+        return false;
     }
-}
+    }
