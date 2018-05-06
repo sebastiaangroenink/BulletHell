a4fd7e0135b8b692a7d9c3396efdbb6f8f26f080
GuyVanDerMeulenDeltion <guy_meulen@live.nl>
2018-05-07 00:53:50 +0200

    Raw Version Boss 6 Skelehands

---
+++ BulletHell/Assets/Scripts/Bosses/Projectiles/ParentProjectile.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*During its lifetime, is able to instantiate projectiles itself*/
+
+public class ParentProjectile : ProjectileTemplate {
+
+    [Header("Parent Projectile Settings:")]
+    public ProjectileTemplate projectiles;
+    public int projectilefireAmount = 2;
+    public float instanceTimerBase = 5;
+    public float angleOffset;
+    public int movementSpeedIncrease = 1;
+
+    [Header("Generation Settings:")]
+    public int maxGenerations = 1;
+
+    [Header("Randomize AngleOffsetSettings:")]
+    public bool randomizeOffset;
+
+    #region Private Variables
+    private int curGeneration;
+    private float instanceTimer;
+    #endregion
+
+    public void Awake() {
+        instanceTimer = instanceTimerBase;
+    }
+
+    public override void Update () {
+        base.Update();
+
+        Fire();
+        Rotation();
+	}
+
+    void Rotation() { //Visual effect;
+        transform.GetChild(0).transform.Rotate(0, 0, 100 * Time.deltaTime);
+    }
+
+
+    public override void SetParameters() {
+        base.SetParameters();
+
+        instanceTimer -= Time.deltaTime;
+    }
+
+    public void Fire() {
+        bool cooldown = CoolDown();
+
+        if (randomizeOffset)
+            angleOffset = Random.Range(0, 360);
+
+        if(cooldown && curGeneration < maxGenerations) {
+            for(int i = 0; i < projectilefireAmount; i++) {
+                GameObject projectile = Instantiate(projectiles.gameObject, transform.position, Quaternion.identity);
+                projectile.GetComponent<ProjectileTemplate>().forwardAxis -= (angleOffset) * Mathf.PI;
+                projectile.GetComponent<ProjectileTemplate>().movementSpeed = movementSpeed + movementSpeedIncrease;
+
+                projectile = Instantiate(projectiles.gameObject, transform.position, Quaternion.identity);
+                projectile.GetComponent<ProjectileTemplate>().forwardAxis += (angleOffset) * Mathf.PI;
+                projectile.GetComponent<ProjectileTemplate>().movementSpeed = movementSpeed + movementSpeedIncrease;
+            }
+        }
+    }
+
+    public bool CoolDown() { //Cooldown for next shot;
+        if(instanceTimer <= 0) {
+            instanceTimer = instanceTimerBase;
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ BulletHell/Assets/Scripts/Bosses/Scripts/Bosses/BossSix/SkeleHand.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*New boss that introduces projectile duplication with main bending projectiles*/
+
+public class SkeleHand : BossTemplate {
+
+    [Header("SkeleHand Settings:")]
+    public int healthTillSecondPhase;
+    public List<Transform> instancePoints; //Used for Instantiating objects;
+    public float bendingOffset = 20;
+
+    [Header("Random Settings:")]
+    public bool randomizeBendingOffset = false;
+    public float minBendingOffset = 10;
+    public float maxBendingOffset = 20;
+
+    [Header("Phase Two Settings:")]
+    public float baseSpecialTimer = 4;
+
+    #region Private Variables
+    private bool secondPhase = false;
+    private float specialTimer;
+    #endregion
+
+    public override void Start() {
+        base.Start();
+
+        specialTimer = baseSpecialTimer;
+    }
+
+    public override void Update() {
+        base.Update();
+
+        specialTimer -= Time.deltaTime;
+    }
+
+    public override void BaseAttack() {
+        base.BaseAttack();
+
+        if (health <= healthTillSecondPhase)
+            secondPhase = true;
+
+        if(cooledDown) {
+            for(int i = 0; i < instancePoints.Count; i++) {
+                GameObject projectile = Instantiate(projectiles[0].gameObject, instancePoints[i].position, Quaternion.identity);
+                projectile.GetComponent<Bending>().movementSpeed = 10;
+                projectile.GetComponent<Bending>().forwardAxis = 180;
+                if(randomizeBendingOffset)
+                projectile.GetComponent<Bending>().bendingAngle = Random.Range(minBendingOffset, maxBendingOffset);
+            }
+        }
+
+        bool specialCooldown = SpecialCooldown();
+
+        if(secondPhase && specialCooldown) {
+            GameObject projectile = Instantiate(projectiles[1].gameObject, transform.position, Quaternion.identity);
+            projectile.GetComponent<ProjectileTemplate>().forwardAxis = 90;
+
+            projectile = Instantiate(projectiles[1].gameObject, transform.position, Quaternion.identity);
+            projectile.GetComponent<ProjectileTemplate>().forwardAxis = 270;
+        }
+    }  
+    
+    bool SpecialCooldown() {
+        if(specialTimer <= 0) {
+            specialTimer = baseSpecialTimer;
+            return true;
+        }
+        return false;
+    } 
+}
